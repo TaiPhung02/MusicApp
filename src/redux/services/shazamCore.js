1d4e9d9d3b15491186d43e58119b39df48016879
TaiDev <2051063445@e.tlu.edu.vn>
2025-03-14 09:43:34 +0700

    feat: song details & add lyrics api

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -19,7 +19,26 @@ export const geniusApi = createApi({
       query: ({ timePeriod = "day", genre = "all", perPage = 10, page = 1 }) =>
         `/chart/songs/?time_period=${timePeriod}&chart_genre=${genre}&per_page=${perPage}&page=${page}`,
     }),
+
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/song/details/?id=${songid}`,
+    }),
   }),
 });
 
-export const { useGetChartsSongQuery } = geniusApi;
+export const { useGetChartsSongQuery, useGetSongDetailsQuery } = geniusApi;
+
+export const lyricsApi = createApi({
+  reducerPath: "lyricsApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api.lyrics.ovh/v1",
+  }),
+  endpoints: (builder) => ({
+    getLyrics: builder.query({
+      query: ({ artist, title }) => `/${artist}/${title}`,
+    }),
+  }),
+});
+
+export const { useGetLyricsQuery } = lyricsApi;
+
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import playerReducer from "./features/playerSlice";
 import { geniusApi } from "./services/shazamCore";
+import { lyricsApi } from "./services/shazamCore";
 
 export const store = configureStore({
   reducer: {
     [geniusApi.reducerPath]: geniusApi.reducer,
+    [lyricsApi.reducerPath]: lyricsApi.reducer,
     player: playerReducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(geniusApi.middleware),
+    getDefaultMiddleware().concat(geniusApi.middleware, lyricsApi.middleware),
 });
