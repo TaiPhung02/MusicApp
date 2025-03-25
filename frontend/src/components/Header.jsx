8254f38d5dc8c0aeb581949eaf15a41e4f6a0c2d
TaiDev <2051063445@e.tlu.edu.vn>
2025-03-25 13:48:16 +0700

    fix: responsive burgur menu

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -6,7 +6,7 @@ const Header = () => {
   return (
     <div className="flex items-center justify-between bg-[#0F0D13] p-4">
       <Searchbar />
-      <div className="flex items-center gap-4">
+      <div className="mr-10 md:mr-0 flex items-center gap-4 ">
         <Notification />
         <User />
       </div>
--- frontend/src/components/Searchbar.jsx
+++ frontend/src/components/Searchbar.jsx
@@ -54,25 +54,25 @@ const Searchbar = () => {
   };
 
   return (
-    <div className="relative w-[375px]">
+    <div className="relative w-[200px] sm:w-[375px]">
       {/* Input Search */}
       <form
         onSubmit={handleSubmit}
         autoComplete="off"
-        className="flex items-center bg-[#26242A] rounded-lg px-4 py-2 transition-all duration-200 hover:bg-[#2E2C34] border-2 border-transparent focus-within:border-[#8A86FF]">
+        className="flex items-center w-full sm:w-[375px] bg-[#26242A] rounded-lg px-4 py-2 transition-all duration-200 hover:bg-[#2E2C34] border-2 border-transparent focus-within:border-[#8A86FF]">
         <FiSearch
           className={`w-5 h-5 ${searchTerm ? "text-white" : "text-gray-400"}`}
         />
         <input
           name="search-field"
           id="search-field"
-          placeholder="Artists, tracks, podcasts..."
+          placeholder="Artists, tracks,..."
           type="text"
           value={searchTerm}
           onChange={handleChange}
           onFocus={() => setShowDropdown(true)}
           onBlur={() => setTimeout(() => setShowDropdown(false), 200)}
-          className="flex-1 bg-transparent border-none outline-none placeholder-gray-500 text-white px-3"
+          className="flex-1 w-full sm:w-auto bg-transparent border-none outline-none placeholder-gray-500 text-white px-3"
         />
         {searchTerm && (
           <button
